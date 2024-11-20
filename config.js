fe5212cff1ab9014d33c77f9007ab274cd397dfe
meltzer5312 <melissalje05@gmail.com>
2024-11-20 15:21:19 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+window.config = {
+  cognito: {
+    userPoolId: 'us-east-1_RaBDdts4K',
+    userPoolClientId: '128mo7dint66674m5c1ci107ma',
+    region: 'us-east-1'
+  },
+  api: {
+    invokeUrl: 'https://of02lqqxil.execute-api.us-east-1.amazonaws.com/dev'
+  }
+};
